06395e10ebe7add7126af9176b318844c6aa0487
qdxblhf <1060532353@qq.com>
2026-07-25 01:03:43 +0800

    chore: ignore wrangler runtime output in lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['**/coverage/**', '**/dist/**', '**/node_modules/**'],
+    ignores: ['**/.wrangler/**', '**/coverage/**', '**/dist/**', '**/node_modules/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
